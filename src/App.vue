8bf420be424e2f90649f1ba90f83c243c35670e1
Yaoe2021 <89341185+Yaoe2021@users.noreply.github.com>
2022-03-05 09:20:37 +0800

    5

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
 @import "./assets/css/base.css";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
 </style>
