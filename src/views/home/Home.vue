5337ac9aeb8f35588b71eb790a994d470a4ce711
Yaoe2021 <89341185+Yaoe2021@users.noreply.github.com>
2022-03-09 23:02:27 +0800

    3

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <h2>HOME</h2>
+  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 </template>
 
 <script>
+import NavBar from '../../components/navbar/NavBar.vue'
 export default {
-
+  components:{
+  NavBar
+  }
 }
 </script>
 
 <style>
-
+.home-nav{
+  background-color: pink;
+  color: white;
+}
 </style>
