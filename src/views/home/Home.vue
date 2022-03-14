3b6a10a00c95ed303d613acb183619fea0263308
Yaoe2021 <89341185+Yaoe2021@users.noreply.github.com>
2022-03-14 11:09:27 +0800

    1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+  </div>
 </template>
 
 <script>
-import NavBar from '../../components/navbar/NavBar.vue'
+import NavBar from "../../components/navbar/NavBar.vue";
+import { getHomeMultidata } from "../../network/home";
+import HomeSwiper from "./childComps/HomeSwiper.vue"
+
 export default {
-  components:{
-  NavBar
-  }
-}
+  components: {
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style>
-.home-nav{
+.home-nav {
   background-color: pink;
   color: white;
 }
